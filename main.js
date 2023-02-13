9dc6bc5aeeb2200b30e4e0c452552e362dd41aab
Ismael Blandon <ismaelbm432@gmail.com>
2023-02-13 13:55:51 -0500

    Portafolio con boton funcional

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ const navToggle = document.querySelector(".button_mobile")
 const navMenu = document.querySelector(".menu")
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(".deploy_menu_visible")
+    navMenu.classList.toggle("deploy_menu_visible")
 })
