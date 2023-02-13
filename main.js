d17b305ff2ec12cae0f79d186908eb67e2f29f23
Ismael Blandon <ismaelbm432@gmail.com>
2023-02-12 23:46:55 -0500

    Mi repositorio Homework

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const navToggle = document.querySelector(".button_mobile")
+const navMenu = document.querySelector(".menu")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle(".deploy_menu_visible")
+})
